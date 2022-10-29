1bc1bb2b524e04850d03bebbfa27cfa0ec30fe1e
Miles7991 <761328939@qq.com>
2022-10-29 17:20:18 -0600

    V1.1 fix some bug for url in _config.tml

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221030.070747';
+const cacheName = 'chirpy-20221029.171857';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
