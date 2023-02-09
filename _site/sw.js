4bad4316c4d84c70a0a6e540a1572e6b29dc12fd
Miles7991 <761328939@qq.com>
2023-02-08 18:06:49 -0700

    2023-02-08-v2-fixed

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230208.175031';
+const cacheName = 'chirpy-20230208.180427';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
